fa19c75082b59c343f67a1f69e02f85702a91207
Navezm <navez.martin@gmail.com>
2021-01-13 12:05:27 +0100

    modification du doc App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import './style.css';
+import coding_img from './img/coding_school.jpg'
+
 function App() {
   return (
-    
+    <div>
+      <h1 className="text-blue">Exercice3</h1>
+      <img src={coding_img}></img>
+    </div>
   );
 }
 
