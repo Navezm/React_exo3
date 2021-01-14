8a7cdf4de5b42a6c1f865fbaa12dded0a0e77ef4
Navezm <navez.martin@gmail.com>
2021-01-14 09:37:47 +0100

    Project finished

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <div>
+      <ul>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>Portfolio</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
